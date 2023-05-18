ab381d40b4e1db186d6bc54e21cd3ef67323d7b5
sarava338 <sarava338@gmail.com>
2023-05-18 20:22:21 +0530

    root element changed to main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
-import "./App.css";
 
 const App = () => {
   return (
