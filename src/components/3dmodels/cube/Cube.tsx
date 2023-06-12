12e4acab5ef84f9c24e0ba7af92ff796da476aa5
sarava338 <sarava338@gmail.com>
2023-06-12 19:15:28 +0530

    cube 3d model used by three.js

---
+++ src/components/3dmodels/cube/Cube.tsx
@@ -0,0 +1,36 @@
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+import CanvasLoader from "../Loader";
+
+const Cube = () => {
+  const cube = useGLTF("../../../../public/cube1.glb");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.5} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={cube.scene} />
+    </mesh>
+  );
+};
+
+const CubeCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={< CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Cube />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default CubeCanvas;
