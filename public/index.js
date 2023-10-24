12149e79ff0685779747f3a877d776362976161b
sarava338 <sarava338@gmail.com>
2023-10-24 22:09:28 +0530

    form setup

--- public/index.js
+++ public/index.js
@@ -1,8 +1,18 @@
-const themeHandler = document.getElementById('theme-handler')
+const themeButton = document.getElementById('theme-handler')
+const form = document.getElementById('form')
+const mailerName = document.getElementById('name')
+const mail = document.getElementById('mail')
+const message = document.getElementById('message')
+
 const root = document.documentElement.style
 
 const DARK_THEME_COLOR = '#333333'
 const BRIGHT_THEME_COLOR = '#fffff1'
+
+const errorHandler = (error) => {
+  console.error(error)
+  alert(error.message)
+}
 
 const setTheme = (theme) => {
   switch (theme) {
@@ -20,11 +30,29 @@ const setTheme = (theme) => {
   }
 }
 
+const sendMail = (name, mail, message) => {
+  console.log(name, mail, message)
+}
+
+const formHandler = (e) => {
+  e.preventDefault()
+  if (mailerName.value && mail.value && message.value) {
+    sendMail(mailerName.value, mail.value, message.value)
+    mailerName.value = ''
+    mail.value = ''
+    message.value = ''
+  } else errorHandler(new Error('type in all inputs'))
+}
+
 document.body.onload = () => {
   setTheme(localStorage.getItem('theme'))
 }
 
-themeHandler.addEventListener('click', () => {
+themeButton.addEventListener('click', () => {
   if (localStorage.getItem('theme') === 'dark') setTheme('bright')
   else setTheme('dark')
+})
+
+form.addEventListener('submit', (e) => {
+  formHandler(e)
 })
