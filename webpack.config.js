375f6c07c35f749c5a2593dbccd0d429e689a7f0
sarava338 <sarava338@gmail.com>
2023-10-28 21:04:26 +0530

    spelling mistake in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     clean: true,
-    assetModuleFileName: '[name][ext]',
+    assetModuleFilename: 'asset/[name][ext]',
   },
   devtool: 'source-map',
   devServer: {
