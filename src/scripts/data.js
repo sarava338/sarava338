f5823643d3d1851ba007fa826ef048ce764ae80c
sarava338 <sarava338@gmail.com>
2023-10-29 16:19:57 +0530

    small color adjustment in sm links

---
+++ src/scripts/data.js
@@ -0,0 +1,38 @@
+export const getSocialMediaData = (color = '333333', size = '50') => [
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/telegram.png`,
+    url: 'https://t.me/sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/instagram-new--v1.png`,
+    url: 'https://www.instagram.com/sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/linkedin.png`,
+    url: 'https://www.linkedin.com/in/sarava338"',
+  },
+  {
+    src: `https://img.icons8.com/${color}/external-tal-revivo-bold-tal-revivo/${size}/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo.png`,
+    url: 'https://www.leetcode.com/sarava338',
+  },
+  {
+    src: `https://img.icons8.com/color/${size}/GeeksforGeeks.png`,
+    url: 'https://auth.geeksforgeeks.org/user/sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/github.png`,
+    url: 'https://www.github.com/sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/facebook-new.png`,
+    url: 'https://www.facebook.com/sarava0338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/youtube-play.png`,
+    url: 'https://www.youtube.com/@sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/coffee-to-go.png`,
+    url: 'https://www.buymeacoffee.com/sarava338',
+  },
+]
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,33 +1,28 @@
-import { DARK_COLOR, BRIGHT_COLOR } from './constants.js'
+import { getSocialMediaData } from './data.js'
 
-const socialMediaElements =
-  document.getElementsByClassName('social_media_links')
+const socialMediaElement = document.getElementById('social_media_links')
 
 const themeButton = document.getElementById('theme-handler').children[0]
 
 export const setTheme = (color) => {
   color = color.slice(1)
-  const size = '50'
-  //https://icons8.com/
-  const socialMediaIcons = [
-    `https://img.icons8.com/${color}/ios-filled/${size}/telegram.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/instagram-new--v1.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/linkedin.png`,
-    `https://img.icons8.com/${color}/external-tal-revivo-bold-tal-revivo/${size}/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo.png`,
-    `https://img.icons8.com/color/${size}/GeeksforGeeks.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/github.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/facebook-new.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/youtube-play.png`,
-    `https://img.icons8.com/${color}/ios-filled/${size}/coffee-to-go.png`,
-  ]
 
   color === '333333'
     ? (themeButton.src = `https://img.icons8.com/${color}/ios-filled/25/crescent-moon.png`)
     : (themeButton.src = `https://img.icons8.com/${color}/ios-filled/25/sun.png`)
 
-  for (let i = 0; i < socialMediaIcons.length; i++) {
-    socialMediaElements[i].src = socialMediaIcons[i]
-  }
+  socialMediaElement.innerHTML = ''
+  getSocialMediaData(color).forEach((data) => {
+    let li = document.createElement('li')
+    let a = document.createElement('a')
+    a.href = data.url
+    a.target = '_blank'
+    let img = document.createElement('img')
+    img.src = data.src
+    a.append(img)
+    li.append(a)
+    socialMediaElement.append(li)
+  })
 }
 
 export const sendMail = (name, mail, message) => {
