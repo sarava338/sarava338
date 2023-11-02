d0795c23d29da6d11c30f111eca8c588485d5fa2
sarava338 <sarava338@gmail.com>
2023-11-02 20:53:53 +0530

    added codepen link

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -2,37 +2,51 @@ export const getSocialMediaData = (color = '333333', size = '50') => [
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/telegram.png`,
     url: 'https://t.me/sarava338',
+    alt: 'telegram@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/instagram-new--v1.png`,
     url: 'https://www.instagram.com/sarava338',
+    alt: 'instagram@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/linkedin.png`,
     url: 'https://www.linkedin.com/in/sarava338"',
+    alt: 'linkedin@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/external-tal-revivo-bold-tal-revivo/${size}/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo.png`,
     url: 'https://www.leetcode.com/sarava338',
+    alt: 'leetcode@sarava338',
   },
-  {
-    src: `https://img.icons8.com/color/${size}/GeeksforGeeks.png`,
-    url: 'https://auth.geeksforgeeks.org/user/sarava338',
-  },
+  // {
+  //   src: `https://img.icons8.com/color/${size}/GeeksforGeeks.png`,
+  //   url: 'https://auth.geeksforgeeks.org/user/sarava338',
+  //   alt: 'geekforgeeks@sarava338',
+  // },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/github.png`,
     url: 'https://www.github.com/sarava338',
+    alt: 'github@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/facebook-new.png`,
     url: 'https://www.facebook.com/sarava0338',
+    alt: 'facebook@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/youtube-play.png`,
     url: 'https://www.youtube.com/@sarava338',
+    alt: 'youtube@sarava338',
+  },
+  {
+    src: `https://img.icons8.com/${color}/ios-filled/${size}/codepen.png`,
+    url: 'https://codepen.io/sarava338',
+    alt: 'codepen@sarava338',
   },
   {
     src: `https://img.icons8.com/${color}/ios-filled/${size}/coffee-to-go.png`,
     url: 'https://www.buymeacoffee.com/sarava338',
+    alt: 'buymeacoffee@sarava338',
   },
 ]
